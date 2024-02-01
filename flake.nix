49e9dbff85672e3c67617fb53d78fad2d11e0480
xenia <xenia.agda@gmail.com>
2024-02-01 21:26:49 +0100

    Include .def file in out/dev, make dev-only package

--- flake.nix
+++ flake.nix
@@ -12,12 +12,19 @@
           asm = fox32asm.packages.${sys}.default;
           fox32rom = pkgs.runCommand "fox32rom" {} ''
             cp -r ${./.}/* ./
-            mkdir -p $out/bin
+            mkdir -p $out/{bin,dev}
             ${asm}/bin/fox32asm ./main.asm $out/bin/fox32.rom
+            cp ${./fox32rom.def} $out/dev/fox32rom.def
+          '';
+
+          fox32rom-dev = pkgs.runCommand "fox32rom-dev" {} ''
+            mkdir -p $out/dev
+            cp ${./fox32rom.def} $out/dev/fox32rom.def
           '';
 
       in rec {
         packages.fox32rom = fox32rom;
+        packages.fox32rom-dev = fox32rom-dev;
         packages.default = fox32rom;
 
         devShells.default = pkgs.mkShell {
